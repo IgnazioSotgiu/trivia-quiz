0d91fe32492164babe04d3b76f7d9d2eec8446b0
IgnazioSotgiu <ignazio_sotgiu@yahoo.it>
2021-02-10 02:03:12 +0000

    start runGame function and changing button color to start

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,54 +1,81 @@
+let questionListArray;
+let difficultyLevel;
+
+// getting the list of question from remote api
+// and store it in a array questionListArray
+function getQuestionList(difficultyLevel){
+    let xhr = new XMLHttpRequest();
+
+    xhr.open("GET" , `https://opentdb.com/api.php?amount=10&category=9&difficulty=${difficultyLevel}&type=boolean`);
+    xhr.send("POST");
+
+    xhr.onreadystatechange = function() {
+        console.log(this.readystate);
+    if(this.readyState === 4 && this.status === 200) {
+        questionListArray = JSON.parse(this.responseText);
+        //console.log(questionListArray);
+        }
+    }
+    return questionListArray;
+}
+
 // wait for the DOM to finish loading page
 
 document.addEventListener("DOMContentLoaded", function() {
     let inputs = document.getElementsByTagName("input");
     console.log(inputs);
 
-    let difficultyLevel;
-
+    
     for(let input of inputs) {
         input.addEventListener("click", function() {
             if (this.getAttribute("value") === "easy") {
                 difficultyLevel = this.getAttribute("value");
                 getQuestionList(difficultyLevel);
                 alert(`You have select the ${difficultyLevel} level`);
+                setTimeout(function() {
+                    console.log(questionListArray);
+                    }, 500);
+                changeStartButtonColor();
             }else if (this.getAttribute("value") === "medium") {
                 difficultyLevel = this.getAttribute("value");
                 getQuestionList(difficultyLevel);
                 alert(`You have select the ${difficultyLevel} level`);
+                setTimeout(function() {
+                    console.log(questionListArray);
+                    }, 500);
+                changeStartButtonColor();
             }else if (this.getAttribute("value") === "hard") {
                 difficultyLevel = this.getAttribute("value");
                 getQuestionList(difficultyLevel);
                 alert(`You have select the ${difficultyLevel} level`);
+                setTimeout(function() {
+                    console.log(questionListArray);
+                    }, 500);
+                changeStartButtonColor();
             }else {
                 alert("Incorrect value. Please select the difficulty level")
-            }
-        })
-    }
-    // getting the list of questions from remote api
-    function getQuestionList(difficultyLevel){
-        let questionListArray;    
-
-        let xhr = new XMLHttpRequest();
-        xhr.onreadystatechange = function() {
-        if(this.readyState === 4 && this.status === 200) {
-            questionListArray = this.responseText;
-            console.log(questionListArray);
-            }
+                }
+            });
         }
-        xhr.open("GET" , `https://opentdb.com/api.php?amount=10&category=9&difficulty=${difficultyLevel}&type=boolean`);
-        xhr.send("POST");
+    });
+
+// set the background of the start button to green 
+// after the selection of difficulty has been made
+function changeStartButtonColor() {
+    $("#start-button").css({"background-color": "#1fe24c"});
+
+}
+let button = document.getElementById("start-button");
+button.addEventListener("click", runGame);
+
+function runGame(questionListArray) {
+    window.location.href = "https://8000-crimson-dog-pol18cu2.ws-eu03.gitpod.io/question-page.html";
+
+    for(let i = 0; i < questionListArray.length; i++) {
+
+        $("#question-number").innerHTML(`Question # ${i+1}`);
+        $("#question-text").innerHTML(questionListArray[i].question).value;
     }
-});
-function difficultyLevel() {
-
-}
-
-function getQuestionArray() {
-
-}
-
-function runGame() {
 
 }
 
@@ -72,12 +99,21 @@ function endOfTestInfo() {
 
 }
 
+/*
 // getting the list of question from remote api
-let xhr = new XMLHttpRequest();
-xhr.onreadystatechange = function() {
+// and store it in a array questionListArray
+function getQuestionList(difficultyLevel){
+    let xhr = new XMLHttpRequest();
+
+    xhr.open("GET" , `https://opentdb.com/api.php?amount=10&category=9&difficulty=${difficultyLevel}&type=boolean`);
+    xhr.send("POST");
+
+    xhr.onreadystatechange = function() {
+        console.log(this.readystate);
     if(this.readyState === 4 && this.status === 200) {
-        questionListArray = this.responseText;
+        questionListArray = JSON.parse(this.responseText);
+        //console.log(questionListArray);
+        }
     }
-}
-xhr.open("GET" , `https://opentdb.com/api.php?amount=50&category=9&difficulty=${difficultyLevel}&type=boolean`);
-xhr.send("POST");
+    return questionListArray;
+}*/
