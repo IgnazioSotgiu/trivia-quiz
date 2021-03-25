4df8e99ff71c4b0d3f8c9077b23019832afc3102
IgnazioSotgiu <ignazio_sotgiu@yahoo.it>
2021-03-25 01:43:51 +0000

    validating contact form

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -23,17 +23,60 @@ function sendMail() {
 let contactBtn = document.getElementById("contact-submit-btn");
 contactBtn.addEventListener("click", checkForm);
 
-function checkForm() {
+function checkForm(event) {
+    event.preventDefault();
     let contactName = document.getElementById("contact-name").value;
     let contactEmail = document.getElementById("contact-email").value;
     let contactEnquiry =document.getElementById("contact-enquiry").value;
 
-    if(contactName && contactEmail && contactEnquiry) {
+    let validEmail = validateEmail(contactEmail);
+
+    if(contactName && validEmail && contactEnquiry) {
         sendMail();
-        alert("Your email has been sent");
+        $(".swal-button, .swal-overlay").css("display", "block");
+        swal({
+            title: "Success!",
+            icon: "success",
+            text: `Your email has been sent`,
+            button: "OK",
+        });
+        setTimeout(function(){
+            $(".swal-button, .swal-overlay").css("display", "none");
+            $("#contact-modal").css("display", "none");
+        }, 2000);
          
     } else {
-        alert("Some fiels are not completed. Please complete the form.");
+        if(!contactName){
+            $(".swal-button, .swal-overlay").css("display", "block");
+            swal({
+                title: "Error!",
+                icon: "info",
+                text: `Please enter your name`,
+                button: "OK",
+            });
+        } else if(!contactEmail) {
+            $(".swal-button, .swal-overlay").css("display", "block");
+            swal({
+                title: "Error!",
+                icon: "info",
+                text: `Please enter a valid email address`,
+                button: "OK",
+            });
+        } else {
+            $(".swal-button, .swal-overlay").css("display", "block");
+            swal({
+                title: "Error!",
+                icon: "info",
+                text: `Please enter a message`,
+                button: "OK",
+            });
+        }
 
     }
 }
+function validateEmail(email) 
+    {
+        var re = /\S+@\S+\.\S+/;
+        return re.test(email);
+    }
+    
